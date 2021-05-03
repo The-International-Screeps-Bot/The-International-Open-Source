95ce9bd9fcc3c83a126c3ac2b7e6ac4d9cb45894
Carson Burke <carsonburke22@gmail.com>
2021-05-03 11:08:30 -0700

    builders use terminal

--- src/role.builder.js
+++ src/role.builder.js
@@ -26,43 +26,56 @@ module.exports = {
             }
             else {
                 
-                let storage = creep.room.storage
+                let terminal = creep.room.terminal
                 
-                if (storage) {
+                if (terminal && terminal.store[RESOURCE_ENERGY] >= 50000) {
                     
-                    creep.say("S 5k")
+                    creep.say("T >= 50k")
                     
-                    let target = storage
-                    
-                    if (target.store[RESOURCE_ENERGY] >= 5000) {
+                    let target = terminal
                         
-                        creep.energyWithdraw(target)
-                    }
+                    creep.energyWithdraw(target)
                 }
                 else {
-
-                    creep.searchSourceContainers()
+                    
+                    let storage = creep.room.storage
+                    
+                    if (storage) {
+                        
+                        creep.say("S 10k")
+                        
+                        let target = storage
+                        
+                        if (target.store[RESOURCE_ENERGY] >= 10000) {
                             
-                        if (creep.container != null && creep.container) {
-                                
-                            creep.say("SC")
-                                
-                            let target = creep.container
-                                
                             creep.energyWithdraw(target)
-                    } else {
-        
-                        let droppedResources = creep.pos.findClosestByRange(FIND_DROPPED_RESOURCES, {
-                            filter: (s) => s.resourceType == RESOURCE_ENERGY && s.energy >= creep.store.getCapacity() * 0.5
-                        });
-        
-                        if (droppedResources) {
-        
-                            creep.say("💡")
-                            
-                            target = droppedResources
-        
-                            creep.pickupDroppedEnergy(target)
+                        }
+                    }
+                    else {
+    
+                        creep.searchSourceContainers()
+                                
+                            if (creep.container != null && creep.container) {
+                                    
+                                creep.say("SC")
+                                    
+                                let target = creep.container
+                                    
+                                creep.energyWithdraw(target)
+                        } else {
+            
+                            let droppedResources = creep.pos.findClosestByRange(FIND_DROPPED_RESOURCES, {
+                                filter: (s) => s.resourceType == RESOURCE_ENERGY && s.energy >= creep.store.getCapacity() * 0.5
+                            });
+            
+                            if (droppedResources) {
+            
+                                creep.say("💡")
+                                
+                                target = droppedResources
+            
+                                creep.pickupDroppedEnergy(target)
+                            }
                         }
                     }
                 }
