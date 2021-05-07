c8f164ad9eb146f012c7ab9527d699d962f20e14
Carson Burke <carsonburke22@gmail.com>
2021-05-06 18:04:37 -0700

    fixed links, remote harvesters and towers do not target scouts and workers of enemies

--- src/module.links.js
+++ src/module.links.js
@@ -8,20 +8,20 @@ module.exports = {
                 let sourceLink1 = Game.getObjectById(room.memory.sourceLink1)
                 let sourceLink2 = Game.getObjectById(room.memory.sourceLink2)
 
-                if (sourceLink1 && controllerLink && sourceLink1.store[RESOURCE_ENERGY] >= 790 && controllerLink.store[RESOURCE_ENERGY] <= 400 && ((room.storage.store[RESOURCE_ENERGY] >= 250000) || (Memory.global.globalStage == 0 && room.storage.store[RESOURCE_ENERGY] >= 5000))) {
+                if (sourceLink1 && controllerLink && sourceLink1.store[RESOURCE_ENERGY] >= 790 && controllerLink.store[RESOURCE_ENERGY] <= 400 && ((room.storage.store[RESOURCE_ENERGY] >= 250000) || (Memory.global.globalStage == 0 && room.storage.store[RESOURCE_ENERGY] >= 15000))) {
 
                     sourceLink1.transferEnergy(controllerLink);
 
-                } else if (sourceLink1 && baseLink && sourceLink1.store[RESOURCE_ENERGY] >= 790 && baseLink.store[RESOURCE_ENERGY] <= 400) {
+                } else if (sourceLink1 && baseLink && sourceLink1.store[RESOURCE_ENERGY] >= 790 && baseLink.store[RESOURCE_ENERGY] <= 700) {
 
                     sourceLink1.transferEnergy(baseLink);
 
                 }
-                if (sourceLink2 && controllerLink && sourceLink2.store[RESOURCE_ENERGY] >= 790 && controllerLink.store[RESOURCE_ENERGY] <= 400 && ((room.storage && room.storage.store[RESOURCE_ENERGY] >= 250000) || (Memory.global.globalStage == 0 && room.storage.store[RESOURCE_ENERGY] >= 5000))) {
+                if (sourceLink2 && controllerLink && sourceLink2.store[RESOURCE_ENERGY] >= 790 && controllerLink.store[RESOURCE_ENERGY] <= 400 && ((room.storage && room.storage.store[RESOURCE_ENERGY] >= 250000) || (Memory.global.globalStage == 0 && room.storage.store[RESOURCE_ENERGY] >= 15000))) {
 
                     sourceLink2.transferEnergy(controllerLink);
 
-                } else if (sourceLink2 && baseLink && sourceLink2.store[RESOURCE_ENERGY] >= 790 && baseLink.store[RESOURCE_ENERGY] <= 400) {
+                } else if (sourceLink2 && baseLink && sourceLink2.store[RESOURCE_ENERGY] >= 790 && baseLink.store[RESOURCE_ENERGY] <= 700) {
 
                     sourceLink2.transferEnergy(baseLink);
 
--- src/role.remoteHarvester.js
+++ src/role.remoteHarvester.js
@@ -10,36 +10,36 @@ module.exports = {
 
             let source1 = Game.getObjectById(creep.room.memory.source1)
             let source2 = Game.getObjectById(creep.room.memory.source2)
-            
+
             if (!source1) {
 
                 let sources = creep.room.find(FIND_SOURCES)[0]
-    
+
                 creep.room.memory.source1 = sources.id
-    
+
             }
             if (!source2) {
 
                 let sources = creep.room.find(FIND_SOURCES)[1]
-                
+
                 if (sources) {
-                
-                creep.room.memory.source2 = sources.id
+
+                    creep.room.memory.source2 = sources.id
                 }
             }
-            
+
             let sourceContainer1 = Game.getObjectById(creep.room.memory.sourceContainer1)
             let sourceContainer2 = Game.getObjectById(creep.room.memory.sourceContainer2)
-            
+
             if (!sourceContainer1) {
 
                 let container = creep.room.find(FIND_STRUCTURES, {
                     filter: s => s.structureType == STRUCTURE_CONTAINER
                 })[0]
-                
+
                 if (container) {
-                
-                creep.room.memory.sourceContainer1 = container.id
+
+                    creep.room.memory.sourceContainer1 = container.id
                 }
             }
             if (!sourceContainer2) {
@@ -47,68 +47,67 @@ module.exports = {
                 let container = creep.room.find(FIND_STRUCTURES, {
                     filter: s => s.structureType == STRUCTURE_CONTAINER
                 })[1]
-    
+
                 if (container) {
-    
-                creep.room.memory.sourceContainer2 = container.id
+
+                    creep.room.memory.sourceContainer2 = container.id
                 }
             }
 
             if (creep.memory.role == "remoteHarvester1") {
-                
+
                 let closestSource = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE)
-                
+
                 if (sourceContainer1 != null) {
-    
+
                     creep.say("⛏️ 1")
-    
+
                     if (creep.pos.inRangeTo(sourceContainer1, 0)) {
-    
+
                         creep.harvest(closestSource)
-    
+
                     } else {
-    
+
                         creep.moveTo(sourceContainer1, { reusePath: 50 })
-    
+
                     }
                 } else {
-    
+
                     creep.moveTo(source1, { reusePath: 50 })
                     creep.harvest(source1);
                     creep.say("⛏️ 3")
-    
+
                 }
-            } 
-            else if (creep.memory.role == "remoteHarvester2") {
-                
+            } else if (creep.memory.role == "remoteHarvester2") {
+
                 let closestSource = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE)
-                
+
                 if (sourceContainer2 != null) {
-    
+
                     creep.say("⛏️ 2")
-    
+
                     if (creep.pos.inRangeTo(sourceContainer2, 0)) {
-    
+
                         creep.harvest(closestSource)
-    
+
                     } else {
-    
+
                         creep.moveTo(sourceContainer2, { reusePath: 50 })
-    
+
                     }
                 } else {
-    
+
                     creep.moveTo(source2, { reusePath: 50 })
                     creep.harvest(source2);
                     creep.say("⛏️ 4")
-    
+
                 }
             }
 
             //targets
             var hostileCreep = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS, {
                 filter: (c) => {
-                    return (AttackWhitelist.indexOf(c.owner.username.toLowerCase()) === -1);
+                    return (AttackWhitelist.indexOf(c.owner.username.toLowerCase()) === -1 && getActiveBodyparts(ATTACK) == 0 && getActiveBodyparts(RANGED_ATTACK) == 0);
                 }
             })
             var hostileStructure = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES, {
@@ -137,15 +136,15 @@ module.exports = {
                 creep.room.memory.builderNeed = true
 
             }
-            
+
             let sources = creep.room.find(FIND_SOURCES)
-            
+
             if (sources.length == 2) {
-                
+
                 for (let remoteRoom of Game.rooms[creep.memory.roomFrom].memory.remoteRooms) {
-                    
+
                     if (remoteRoom.name == creep.room.name) {
-                        
+
                         remoteRoom.sources = 2
                     }
                 }
