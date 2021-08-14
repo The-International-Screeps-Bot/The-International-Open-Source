fae8c461281706c20c6d85d95fdd413eddfd417b
Carson Burke <carsonburke22@gmail.com>
2021-08-13 22:38:27 -0700

    enabled pixel selling

--- src/terminals.js
+++ src/terminals.js
@@ -103,7 +103,7 @@ function terminals(room, terminal) {
 
     if (Game.resources.pixel > 10) {
 
-        //Game.market.createOrder({ type: ORDER_SELL, resourceType: PIXEL, price: avgPrice(PIXEL) * 0.9, totalAmount: 10, roomName: "E25N2" })
+        Game.market.createOrder({ type: ORDER_SELL, resourceType: PIXEL, price: avgPrice(PIXEL) * 0.9, totalAmount: 10, roomName: room.name })
     }
 
     if (Memory.global.globalStage <= 2 && Game.market.credits >= 100000 && terminal.store[RESOURCE_ENERGY] <= 100000 && room.controller.level <= 7) {
