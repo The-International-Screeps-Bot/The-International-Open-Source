eea24ecb30b2d350b9ee700bd7423bcf2c1aa67d
Carson Burke <carsonburke22@gmail.com>
2021-10-15 22:32:05 -0700

    Optimized lab chemistry

--- src/labs.js
+++ src/labs.js
@@ -4,19 +4,38 @@ module.exports = function labs(room) {
 
     let labs = room.get("labs")
 
+    // Stop if there are no labs
+
     if (labs.length == 0) return
 
     let primaryLabs = room.get("primaryLabs")
     let secondaryLabs = room.get("secondaryLabs")
     let tertiaryLabs = room.get("tertiaryLabs")
 
-    if (primaryLabs && primaryLabs.length > 0 && secondaryLabs) {
-        if (secondaryLabs[0].cooldown == 0) {
-            for (let secondaryLab of secondaryLabs) {
+    runLabChemistry()
 
-                secondaryLab.runReaction(primaryLabs[0], primaryLabs[1])
+    function runLabChemistry() {
 
-            }
+        // Stop if there aren't 2 primaryLabs
+
+        if (primaryLabs.length != 2) return
+
+        // Stop if there are no secondaryLabs
+
+        if (secondaryLabs.length == 0) return
+
+        // Stop a secondaryLab is on cooldown
+
+        if (secondaryLabs[0].cooldown > 0) return
+
+        // Loop through secondary labs
+
+        for (let secondaryLab of secondaryLabs) {
+
+            // Run reaction in lab using resources from primaryLabs 1 and 2
+
+            secondaryLab.runReaction(primaryLabs[0], primaryLabs[1])
+
         }
     }
 }
