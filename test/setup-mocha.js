e344473e53fe5909a88157b6cb3af5ab66240859
Pieter <pieterbrandsen2@gmail.com>
2022-05-18 22:18:21 +0200

    Removed test folder

--- test/setup-mocha.js
+++
@@ -1,9 +0,0 @@
-//inject mocha globally to allow custom interface refer without direct import - bypass bundle issue
-global._ = require('lodash');
-global.mocha = require('mocha');
-global.chai = require('chai');
-global.sinon = require('sinon');
-global.chai.use(require('sinon-chai'));
-
-// Override ts-node compiler options
-process.env.TS_NODE_PROJECT = 'tsconfig.test.json'
