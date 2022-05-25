249f246200c858ed0d73f9a0969816cf382f7aba
PieterBrandsen <pieterbrandsen2@gmail.com>
2022-05-25 11:17:07 +0200

    Added constant username gathering from memory

--- src/international/constants.ts
+++ src/international/constants.ts
@@ -74,7 +74,7 @@ interface Constants {
 export const constants: Partial<Constants> = {}
 
 // constants.me = Memory.me
-constants.me = 'MarvinTMB'
+constants.me = Memory.me
 constants.mmoShardNames = new Set(['shard0', 'shard1', 'shard2', 'shard3'])
 
 export const allyList = new Set(['Q13214', 'HerrKai', 'clarkok', 'PandaMaster'])
