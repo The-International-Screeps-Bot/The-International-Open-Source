2a7c3e998bc63c7cbd5580fe40120a3dcb8e27f8
Pieter <pieterbrandsen2@gmail.com>
2022-07-15 23:14:38 +0200

    Buy MORE pixels!

--- src/other/PandaMaster/Market.ts
+++ src/other/PandaMaster/Market.ts
@@ -1,7 +1,7 @@
 export default class Market {
      private _minCpuUnlockSellPrice: number = 50 * 1000 * 1000
 
-     private _maxPixelBuyPrice: number = 30 * 1000
+     private _maxPixelBuyPrice: number = 40 * 1000
 
      private _mainShard: string
 
@@ -44,10 +44,30 @@ export default class Market {
           }
      }
 
+     private BuyMorePixels(): void {
+          const orders = Game.market.getAllOrders(
+               order =>
+                    order.resourceType === PIXEL && order.type === ORDER_BUY && order.price < this._maxPixelBuyPrice,
+          )
+          const myOrder = orders.find(order => order.id === '62d1d72a3a08f134005f736a')
+
+          // highest first
+          orders.sort((a, b) => b.price - a.price)
+          if (orders[0].id !== myOrder.id) {
+               const newPrice = orders[0].price + 1
+               Game.market.changeOrderPrice(myOrder.id, newPrice)
+          }
+
+          if (myOrder.remainingAmount < 1000) {
+               Game.market.extendOrder(myOrder.id, 1000)
+          }
+     }
+
      public HandleOrderEveryTick(): void {
           if (Game.shard.name === this._mainShard) {
                //  this.SellCpuUnlock()
                this.BuyPixels()
+               this.BuyMorePixels()
           }
      }
 }
