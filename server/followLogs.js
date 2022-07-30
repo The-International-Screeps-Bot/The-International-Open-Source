7356c6b92b9b49fbf6b0d4259ee533b2b5281510
PieterBrandsen <pieterbrandsen2@gmail.com>
2022-07-30 16:12:42 +0200

    Forgot to remove logConsole function

--- server/followLogs.js
+++ server/followLogs.js
@@ -1,10 +1,10 @@
-const { logConsole, followLog, setHostname } = require("./helper");
+const { followLog, setHostname } = require("./helper");
 const { playerRoom, rooms } = require("./config");
 
 async function main() {
   if (process.argv.length > 2) {
     setHostname(process.argv[2]);
   }
-  followLog(rooms, logConsole, undefined, playerRoom);
+  followLog(rooms, undefined, playerRoom);
 }
 main();
