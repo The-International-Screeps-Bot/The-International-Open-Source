9426de0d9753ef47fd5e19d3c97fe04dad38ef16
Carson Burke <carsonburke22@gmail.com>
2022-08-07 01:12:44 -0400

    Basic factory code

--- src/room/factory.ts
+++ src/room/factory.ts
@@ -1,5 +1,4 @@
-Room.prototype.factoryManager = function() {
-
+Room.prototype.factoryManager = function () {
     const factory = this.structures.factory[0]
     if (!factory) return
 
@@ -8,14 +7,24 @@ Room.prototype.factoryManager = function() {
     if (factory.createBatteries()) return
 }
 
-StructureFactory.prototype.createBatteries = function() {
+StructureFactory.prototype.createBatteries = function () {
+    // If there is some energy
 
+    if (this.store.energy >= 10000)
+        // Convert energy into batteries
+
+        return this.produce(RESOURCE_BATTERY) === OK
 
     return true
 }
 
-StructureFactory.prototype.createEnergy = function() {
+StructureFactory.prototype.createEnergy = function () {
+    // If there are some batteries and not much energy
 
+    if (this.store.battery >= 1000 && this.store.energy < 20000)
+        // Convert batteries into energy
+
+        return this.produce(RESOURCE_ENERGY) === OK
 
     return true
 }
