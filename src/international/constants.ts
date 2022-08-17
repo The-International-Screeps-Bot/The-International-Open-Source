aab8decc1358c74c7e2157ace7c09bf8a8d95fc3
Carson Burke <carsonburke22@gmail.com>
2022-08-17 12:17:38 -0700

    (breaking) Late fix to breaking version

--- src/international/constants.ts
+++ src/international/constants.ts
@@ -1,7 +1,7 @@
 /**
  * Increment by 1 when a change has been made that will break previous versions of the bot
  */
-export const breakingVersion = 75
+export const breakingVersion = 76
 
 // Settings
 
