030e230883d4b525b79f2c3d8cdad5bd1e2bda2e
Plaid Rabbit <Plaid Rabbit@github>
2022-09-21 13:46:25 -0500

    Clear out the HH manager if it gets strange resources stuck in it.

--- src/room/creeps/roleManagers/commune/hubHaulerManager.ts
+++ src/room/creeps/roleManagers/commune/hubHaulerManager.ts
@@ -40,6 +40,14 @@ export class HubHauler extends Creep {
 
         if (!storage && !terminal) return
 
+        //Whenever we have a reservation, we should have a matching withdraw and transfer, so we should never
+        // get here with anything.  If we do, it'll never be gotten rid of, so just transfer anything we have to the store
+        if(this.store.getUsedCapacity() > 0) {
+            const resource = Object.keys(this.store)[0] as ResourceConstant
+            this.createReservation('transfer', storage.id, this.store[resource], resource)
+            return
+        }
+
         //Factory-overfill is at the top of this list because it can be feeding energy to the rest of the base
         // by breaking down batteries... this is the only case it should have more then 10k energy in the factory.
         if (this.factoryEnergyOverfillTransfer()) return
