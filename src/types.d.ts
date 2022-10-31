0aef5f972f653252d47404a182723aad89f447a6
Aerics <sascha.licht@realityworld.de>
2022-10-31 15:43:33 +0100

    deleteClaimRequests can delete all or one specific

--- src/types.d.ts
+++ src/types.d.ts
@@ -2198,7 +2198,7 @@ declare global {
              */
             claim(requestName: string, communeName?: string): string
 
-            deleteClaimRequests(): string
+            deleteClaimRequests(requestName?: string): string
 
             /**
              * Responds, or if needed, creates, an attack request for a specified room, by a specified room
