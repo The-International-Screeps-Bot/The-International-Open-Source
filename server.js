2ed439ce022bab764539434a0927926d3b08d2d3
Pieter Brandsen <pieterbrandsen2@gmail.com>
2022-12-20 11:10:31 +0100

    Changed server starting sequence

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
-const fs = require('fs')
 const { join } = require('path')
 const { execSync } = require('child_process')
 require('dotenv').config()
 
 const options = { stdio: 'inherit' }
-function Start() {
-    const botPath = join(__dirname, 'dist')
-    execSync('npm run build', options)
-    execSync(`npx screeps-grafana private`, options)
-    execSync(
-        `npx screeps-performance-server ${process.argv[2]} ${botPath} ${process.env.STEAM_KEY} ${process.env.EXPORT_API_BASE_URL}`,
-        options,
-    )
-}
-Start()
-process.exit(0)
+const botPath = join(__dirname, 'dist')
+exec(`npx screeps-grafana private`, options)
+execSync('npm run build', options)
+execSync(
+    `npx screeps-performance-server ${process.argv[2]} ${botPath} ${process.env.STEAM_KEY} ${process.env.EXPORT_API_BASE_URL}`,
+    options,
+)
