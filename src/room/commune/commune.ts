fd50c49d5f68dd3d28e8d16c7d67179ee5776bad
Aerics <sascha.licht@realityworld.de>
2022-12-27 14:37:33 +0100

    added droppedResourceManager for communes

--- src/room/commune/commune.ts
+++ src/room/commune/commune.ts
@@ -47,6 +47,7 @@ import { HaulerSizeManager } from './haulerSize'
 import { HaulerNeedManager } from './haulerNeed'
 import { packXYAsCoord, unpackCoord, unpackPosList } from 'other/packrat'
 import { ContainerManager } from '../container'
+import { DroppedResourceManager } from '../commune/droppedResources'
 import { StoringStructuresManager } from './storingStructures'
 
 export class CommuneManager {
@@ -202,6 +203,7 @@ export class CommuneManager {
         this.haulerNeedManager.run()
 
         this.room.roomManager.containerManager.run()
+        this.room.roomManager.droppedResourceManager.run()
         this.spawningStructuresManager.createRoomLogisticsRequests()
         this.storingStructuresManager.run()
         this.room.linkManager()
@@ -392,7 +394,6 @@ export class CommuneManager {
     _storingStructures: (StructureStorage | StructureTerminal)[]
 
     get storingStructures() {
-
         if (this._storingStructures) return this._storingStructures
 
         this._storingStructures = []
