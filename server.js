5c530eef52a696a939514a217d4cb1efc75f8e12
Pieter Brandsen <pieterbrandsen2@gmail.com>
2023-01-15 18:13:23 +0100

    Updated testing of bots

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const { join } = require('path')
 const { exec, execSync } = require('child_process')
 require('dotenv').config()
+const minimist = require('minimist')
+const argv = minimist(process.argv.slice(2))
+
+const runnerName = process.env.ACTIONS_RUNNER_NAME;
+console.log("runnerName: " + runnerName);
 
 const options = { stdio: 'inherit' }
 const botPath = join(__dirname, 'dist')
 exec(`npx screeps-grafana private`, options)
 execSync('npm run build', options)
 execSync(
-    `npx screeps-performance-server ${process.argv[2]} ${botPath} ${process.env.STEAM_KEY} ${process.env.EXPORT_API_BASE_URL}`,
+    `npx screeps-performance-server --maxTicks=${argv.maxTicks} --botFilePath=${botPath} --steamKey=${process.env.STEAM_KEY} --exportBaseUrl=${process.env.EXPORT_API_BASE_URL}`,
     options,
 )
