aff24a4540fb4d4421a6f1d25b810b4b8a4a4b03
Pieter Brandsen <pieterbrandsen2@gmail.com>
2023-01-15 22:36:24 +0100

    Able to push to server

--- server.js
+++ server.js
@@ -7,11 +7,16 @@ const argv = minimist(process.argv.slice(2))
 const runnerName = process.env.ACTIONS_RUNNER_NAME;
 console.log("runnerName: " + runnerName);
 
+const grafanaPort = 3000
+const relayPort = 2003
+const serverPort = 21025
+const cliPort = 21026
+
 const options = { stdio: 'inherit' }
 const botPath = join(__dirname, 'dist')
-exec(`npx screeps-grafana private`, options)
+exec(`npx screeps-grafana private --grafanaPort=${grafanaPort} --relayPort=${relayPort}`, options)
 execSync('npm run build', options)
 execSync(
-    `npx screeps-performance-server --maxTicks=${argv.maxTicks} --botFilePath=${botPath} --steamKey=${process.env.STEAM_KEY} --exportBaseUrl=${process.env.EXPORT_API_BASE_URL}`,
+    `npx screeps-performance-server --maxTicks=${argv.maxTicks} --botFilePath=${botPath} --steamKey=${process.env.STEAM_KEY} --exportBaseUrl=${process.env.EXPORT_API_BASE_URL} --serverPort=${serverPort} --cliPort=${cliPort} --grafanaPort=${grafanaPort}`,
     options,
 )
