38b2bd733d167284d8b4593e57e965274186c362
Carson Burke <carsonburke22@gmail.com>
2023-02-12 14:49:05 -0800

    hauler size change improvements

--- src/room/commune/haulerSize.ts
+++ src/room/commune/haulerSize.ts
@@ -18,14 +18,18 @@ export class HaulerSizeManager {
 
         const avgCPUUsagePercent = (Memory.stats.cpu.usage || 20) / Game.cpu.limit
 
-        // Use to average energy usage percent and the energy capacity availible in the room to determine the max hauler size
-
-        roomMemory.MHC =
+        const newMinHaulerCost =
             (Math.floor(
                 Math.max(Math.pow(avgCPUUsagePercent, 1.3) - 0.4, 0) *
                     Math.min(this.communeManager.room.energyCapacityAvailable / BODYPART_COST.move, MAX_CREEP_SIZE),
             ) +
                 this.communeManager.room.structures.spawn.length * 2) *
             BODYPART_COST[CARRY]
+
+        let diff
+        if (newMinHaulerCost < roomMemory.MHC) diff = (newMinHaulerCost - roomMemory.MHC) / 3
+        else diff = newMinHaulerCost - roomMemory.MHC
+
+        roomMemory.MHC += Math.floor(diff)
     }
 }
