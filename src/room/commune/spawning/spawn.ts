ce3f9586cdde874fa204117b6dc9979e9d249c50
Araele <albert.ravaru@gmail.com>
2023-02-15 21:56:19 +0200

    optimized the script as such: - destructured imports - Instead of importing everything from the modules, you can destructure only the variables that you need. This can improve the performance and make the code more readable. - Use object shorthand notation - Instead of repeating spawnRequest in the advancedSpawn function, you can use the shorthand notation to make the code more concise. - Use template literals instead of concatenation - Instead of using concatenation to construct the string for the creep name, you can use template literals to make the code more concise and easier to read. - Use a constant for the separator - Instead of hardcoding the separator between the components of the creep name, you can use a constant to make the code more maintainable.

--- src/room/commune/spawning/spawn.ts
+++ src/room/commune/spawning/spawn.ts
@@ -1,20 +1,20 @@
-import { creepRoles, customColors, partsByPriority } from 'international/constants'
+import { creepRoles } from 'international/constants'
 import { internationalManager } from 'international/international'
 import { customLog, newID } from 'international/utils'
 
+const CREEP_NAME_SEPARATOR = '_';
+
 StructureSpawn.prototype.testSpawn = function (spawnRequest, ID) {
-    return this.spawnCreep(spawnRequest.body, ID.toString(), { dryRun: true })
+    return this.spawnCreep(spawnRequest.body, ID.toString(), { dryRun: true });
 }
 
 StructureSpawn.prototype.advancedSpawn = function (spawnRequest, ID) {
-
-    spawnRequest.extraOpts.energyStructures = this.room.spawningStructuresByPriority
+    const { body, role, cost, defaultParts, extraOpts } = spawnRequest;
+    extraOpts.energyStructures = this.room.spawningStructuresByPriority;
 
     return this.spawnCreep(
-        spawnRequest.body,
-        `${creepRoles.indexOf(spawnRequest.role)}_${spawnRequest.cost}_${this.room.name}_${
-            spawnRequest.defaultParts
-        }_${ID}`,
-        spawnRequest.extraOpts,
-    )
+      body,
+      `${creepRoles.indexOf(role)}${CREEP_NAME_SEPARATOR}${cost}${CREEP_NAME_SEPARATOR}${this.room.name}${CREEP_NAME_SEPARATOR}${defaultParts}${CREEP_NAME_SEPARATOR}${ID}`,
+      extraOpts,
+    );
 }
