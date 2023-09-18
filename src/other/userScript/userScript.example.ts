19d8ca29e0a63191c88bac216395af726e0c3079
Pieter <pieterbrandsen2@gmail.com>
2023-09-18 13:14:46 +0000

    Added panda's script as example

--- src/other/userScript/userScript.example.ts
+++ src/other/userScript/userScript.example.ts
@@ -1,7 +1,8 @@
 // IMPORTANT: If you want to use the userScript, copy this file, name it userScript.ts, and rename userScriptExample() to userScript()
-global.userScriptExample = function() {
-
-    // Your custom code here
-
-
+global.userScriptExample = function () {
+    console.log('Game.time=' + Game.time)
+    console.log(JSON.stringify(Game.market.getAllOrders()))
+    if (Game.time % 100 === 0) {
+        console.log(JSON.stringify(Game.market.getHistory()))
+    }
 }
