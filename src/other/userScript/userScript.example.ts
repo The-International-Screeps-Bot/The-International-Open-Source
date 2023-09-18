6d05d3542840470f3c4b410e8f8e76d877e5ba9b
Carson Burke <carsonburke22@gmail.com>
2023-09-18 12:08:25 -0700

    Revert "Added panda's script as example"
    This reverts commit 19d8ca29e0a63191c88bac216395af726e0c3079.

--- src/other/userScript/userScript.example.ts
+++ src/other/userScript/userScript.example.ts
@@ -1,8 +1,7 @@
 // IMPORTANT: If you want to use the userScript, copy this file, name it userScript.ts, and rename userScriptExample() to userScript()
-global.userScriptExample = function () {
-    console.log('Game.time=' + Game.time)
-    console.log(JSON.stringify(Game.market.getAllOrders()))
-    if (Game.time % 100 === 0) {
-        console.log(JSON.stringify(Game.market.getHistory()))
-    }
+global.userScriptExample = function() {
+
+    // Your custom code here
+
+
 }
