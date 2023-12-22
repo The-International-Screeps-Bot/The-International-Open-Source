068b14a7c6c395d6d8503f508191a0b5e219929b
Pieter <pieterbrandsen2@gmail.com>
2023-12-22 15:33:59 +0000

    Fixed build files for server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const { join } = require('path')
 const { execSync } = require('child_process')
 require('dotenv').config()
 const minimist = require('minimist')
+const fs = require('fs')
 
 const argv = minimist(process.argv.slice(2))
 
@@ -25,19 +26,33 @@ function getPorts() {
 const ports = getPorts()
 
 const options = { stdio: 'inherit' }
+const srcPath = join(__dirname, 'src')
 const botPath = join(__dirname, 'dist')
 console.log('START')
 // execSync(
 //     `npx screeps-grafana-go_carbon setup && npx screeps-grafana-go_carbon --grafanaPort=${ports.grafanaPort} --relayPort=${ports.relayPort} --force ${argv.debug ? '--debug' : ''} --deleteLogs --deleteWhisper`,
 //     options,
 // )
+
+function customCopyFile(src, dest, searchText, replaceText) {
+    const text = fs.readFileSync(src, 'utf8')
+
+    const regex = new RegExp(searchText, 'g')
+    const result = text.replace(regex, replaceText)
+    fs.writeFileSync(dest, result, 'utf8')
+}
+
+customCopyFile(join(srcPath,"settings.example.ts"), join(srcPath,"settings.ts"),'Example','')
+customCopyFile(join(srcPath,"other/userScript/userScript.example.ts"), join(srcPath,"other/userScript/userScript.ts"),'Example','')
 execSync('npm run build', options)
+
+const cmdString = `npx screeps-performance-server --maxTickCount=${argv.maxTicks || 20000} --maxBots=10 --botFilePath=${botPath} --steamKey=${
+    process.env.STEAM_KEY
+} --exportUrl=${process.env.EXPORT_API_URL} --serverPort=${ports.serverPort} --cliPort=${ports.cliPort} --force ${
+    argv.debug ? '--debug' : ''
+} --deleteLogs --tickDuration=${argv.tickDuration || 250} --logFilter='Error:'`;
 execSync(
-    `npx screeps-performance-server --maxTickCount=${argv.maxTicks || 20000} --maxBots=10 --botFilePath=${botPath} --steamKey=${
-        process.env.STEAM_KEY
-    } --exportUrl=${process.env.EXPORT_API_URL} --serverPort=${ports.serverPort} --cliPort=${ports.cliPort} --force ${
-        argv.debug ? '--debug' : ''
-    } --deleteLogs --tickDuration=${argv.tickDuration || 250} --logFilter='Error:'`,
+    cmdString,
     options,
 )
 // if (argv.stopGrafana) execSync('npx screeps-grafana stop')
