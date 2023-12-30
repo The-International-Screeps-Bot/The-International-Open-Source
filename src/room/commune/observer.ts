6b1f4ec9398460b10aac3169f38fde79386964be
CarsonBurke <carsonburke22@gmail.com>
2023-12-29 22:23:38 -0800

    potential observer optimizations

--- src/room/commune/observer.ts
+++ src/room/commune/observer.ts
@@ -1,81 +1,67 @@
 import { customLog } from 'utils/logging'
-import { randomTick } from 'utils/utils'
+import { forRoomNamesAroundRangeXY, randomTick, utils } from 'utils/utils'
 import { CommuneManager } from './commune'
 import { RoomMemoryKeys } from 'international/constants'
 import { roomNameUtils } from 'room/roomNameUtils'
+import { roomUtils } from 'room/roomUtils'
 
 export class ObserverManager {
-    communeManager: CommuneManager
-    scoutTarget: string
-    observer: StructureObserver
+  communeManager: CommuneManager
+  scoutTarget: string
+  observer: StructureObserver
 
-    constructor(communeManager: CommuneManager) {
-        this.communeManager = communeManager
+  constructor(communeManager: CommuneManager) {
+    this.communeManager = communeManager
+  }
+
+  preTickRun() {
+    // Run only every so often
+    if (!utils.isTickInterval(10)) return
+
+    this.observer = this.communeManager.room.roomManager.structures.observer[0]
+    if (!this.observer) return
+
+    const scoutTarget = this.findScoutTarget()
+    if (!scoutTarget) return
+
+    this.observer.observeRoom(scoutTarget)
+  }
+
+  findScoutTarget() {
+    const roomCoord = roomNameUtils.pack(this.communeManager.room.name)
+
+    let scoutTarget: string | undefined
+    let highestScore = 0
+
+    forRoomNamesAroundRangeXY(roomCoord.x, roomCoord.y, OBSERVER_RANGE, (x, y) => {
+      const roomName = roomNameUtils.unpackXY(x, y)
+
+      const score = this.findRoomNameScore(roomName)
+      if (score <= highestScore) return
+
+      highestScore = score
+      scoutTarget = roomName
+    })
+
+    return scoutTarget
+  }
+
+  private findRoomNameScore(roomName: string) {
+    const roomsDistance =
+      OBSERVER_RANGE - Game.map.getRoomLinearDistance(roomName, this.communeManager.room.name)
+
+    const roomMemory = Memory.rooms[roomName]
+    if (!roomMemory) {
+      const score = roomsDistance * 1000
+      return score
     }
 
-    preTickRun() {
-        // Run only every so often
+    // We have memory of the room. Consider when we last scouted into the score
 
-        if (!randomTick()) return
+    // The time that's surpassed since we last scouted the room
+    const scoutDifference = Game.time - (roomMemory[RoomMemoryKeys.lastScout] || 0)
 
-        this.observer = this.communeManager.room.roomManager.structures.observer[0]
-        if (!this.observer) return
-
-        this.findScoutTarget()
-        if (!this.scoutTarget) return
-
-        this.observer.observeRoom(this.scoutTarget)
-    }
-
-    findScoutTarget() {
-        const roomCoord = roomNameUtils.pack(this.communeManager.room.name)
-
-        let highestScore = 0
-
-        let x1 = roomCoord.x - 10
-        let y1 = roomCoord.y - 10
-        let x2 = roomCoord.x + 10
-        let y2 = roomCoord.y + 10
-
-        for (let x = x1; x <= x2; x += 1) {
-            for (let y = y1; y <= y2; y += 1) {
-                const roomName = roomNameUtils.unpackXY(x, y)
-
-                // Ensure the statuses are the same
-
-                if (
-                    Game.map.getRoomStatus(roomName).status !==
-                    Game.map.getRoomStatus(this.communeManager.room.name).status
-                )
-                    continue
-
-                let score
-
-                if (!Memory.rooms[roomName]) {
-                    score =
-                        (OBSERVER_RANGE -
-                            Game.map.getRoomLinearDistance(
-                                roomName,
-                                this.communeManager.room.name,
-                            )) *
-                        1000
-                } else {
-                    score =
-                        Game.time -
-                        (Memory.rooms[roomName][RoomMemoryKeys.lastScout] || 0) +
-                        (OBSERVER_RANGE -
-                            Game.map.getRoomLinearDistance(
-                                roomName,
-                                this.communeManager.room.name,
-                            )) *
-                            10
-                }
-
-                if (score <= highestScore) continue
-
-                highestScore = score
-                this.scoutTarget = roomName
-            }
-        }
-    }
+    const score = scoutDifference + roomsDistance * 10
+    return score
+  }
 }
