88385f6122582a6ad8ea906895596e2271bcea6e
CarsonBurke <carsonburke22@gmail.com>
2024-01-30 13:54:13 -0800

    new setup for Antifa logic

---
+++ src/room/creeps/roleManagers/antifa/meleeSingleOps.ts
@@ -0,0 +1,22 @@
+export class MeleeSingleSquadOps {
+  registerCommuneDefenceTargets(creep: Creep) {
+
+
+  }
+  defendCommune(creep: Creep) {
+
+
+  }
+  defendCommuneWithRamparts(creep: Creep) {
+
+
+  }
+  findDefenceRampart(creep: Creep) {
+
+
+  }
+  defendCommuneWithoutRamparts(creep: Creep) {
+
+
+  }
+}
